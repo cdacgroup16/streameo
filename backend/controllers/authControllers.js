c70e986dd6b15a58fead6c7649f11813772fbd6d
aadarshp31 <adarshpandey.in@gmail.com>
2021-01-14 00:41:28 +0530

    Added controller to handle user signup at the backend

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,7 +1,12 @@
 const User = require('../models/user')
 const asyncHandler = require('express-async-handler')
 const { generateJwtToken } = require('../utils/jwt')
+const { createUser } = require('./userControllers')
+const { validateEmail, validatePassword } = require('../utils/validation')
 
+// @desc    Lets a user login
+// @route   POST /api/signin
+// @access  Public
 exports.signin = asyncHandler(async (req, res) => {
   let { email, password } = req.body
 
@@ -37,3 +42,30 @@ exports.signin = asyncHandler(async (req, res) => {
     })
   })
 })
+
+// @desc    Registers a new user
+// @route   POST /api/signup
+// @access  Public
+exports.signup = (req, res) => {
+  // Validation
+  let { firstname, email, password } = req.body
+  firstname = firstname && firstname.toLowerCase()
+  email = email && email.toLowerCase()
+  if (!firstname || !email || !password) {
+    res.status(422)
+    throw new Error("Validation failed: Required fields can't be empty")
+  }
+  if (firstname.length < 3) {
+    res.status(422)
+    throw new Error("Validation failed: Firstname can't be smaller than 3 characters")
+  }
+  if (!validateEmail(email)) {
+    res.status(422)
+    throw new Error('Validation failed: Email is not properly formatted')
+  }
+  if (!validatePassword(password)) {
+    res.status(422)
+    throw new Error('Validation failed: Password must contain atleast one lowercase, one uppercase character and atleast one number')
+  }
+  await createUser(req,res)
+}
