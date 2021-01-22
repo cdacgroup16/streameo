c08de0efd26b58a6b0e9557d7f533ff0f0da493d
Rishabh Aggarwal <ri.sbh23@GMAIL.COM>
2021-01-22 22:46:16 +0530

    Entity Class Created

--- frontend/src/app/entities/plans.ts
+++ frontend/src/app/entities/plans.ts
@@ -1,3 +1,15 @@
 export class Plans {
-  
+  name: string;
+  price: number;
+  stream: number;
+  validity: number;
+  max_quality: number;
+
+  constructor(name: string, price: number, stream: number, validity: number, max_quality: number) {
+    this.name = name;
+    this.price = price;
+    this.stream = stream;
+    this.validity = validity;
+    this.max_quality = max_quality;
+  }
 }
