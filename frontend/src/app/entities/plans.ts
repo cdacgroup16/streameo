1e7d1e0b970126b02527f1f92a81a157cfe6bd32
Rishabh Aggarwal <ri.sbh23@GMAIL.COM>
2021-01-23 00:06:29 +0530

    updated ID

--- frontend/src/app/entities/plans.ts
+++ frontend/src/app/entities/plans.ts
@@ -1,14 +1,16 @@
 export class Plans {
+  id: string;
   name: string;
   price: number;
-  stream: number;
+  concurrent_streams: number;
   validity: number;
   max_quality: number;
 
-  constructor(name: string, price: number, stream: number, validity: number, max_quality: number) {
+  constructor(id: string, name: string, price: number, concurrent_streams: number, validity: number, max_quality: number) {
+    this.id = id;
     this.name = name;
     this.price = price;
-    this.stream = stream;
+    this.concurrent_streams = concurrent_streams;
     this.validity = validity;
     this.max_quality = max_quality;
   }
