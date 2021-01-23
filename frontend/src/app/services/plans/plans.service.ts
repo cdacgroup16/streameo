82ecc9de8cbc614039b7ca06e520bc6ca3686a83
Rishabh Aggarwal <ri.sbh23@GMAIL.COM>
2021-01-23 15:24:56 +0530

    Created Service Plans

--- frontend/src/app/services/plans/plans.service.ts
+++ frontend/src/app/services/plans/plans.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,30 @@ import { Injectable } from '@angular/core';
 })
 export class PlansService {
 
-  constructor() { }
+  url: string = "http://localhost:5000/api/plans";
+
+  constructor(private http: HttpClient) { }
+
+  getAllPlans(): any {
+    return this.http.get(this.url);
+  }
+
+  getPlan(id): any {
+    const tempUrl = this.url + "/" + id;
+    return this.http.get(tempUrl);
+  }
+
+  postNewPlan(pObj): any {
+    return this.http.post(this.url, pObj);
+  }
+
+  UpdatePlan(pObj): any {
+    return this.http.put(this.url, pObj);
+  }
+
+  deletePlan(id): any {
+    const tempUrl = this.url + "/" + id;
+    return this.http.delete(tempUrl);
+  }
+
 }
