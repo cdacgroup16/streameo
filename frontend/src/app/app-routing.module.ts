be340b28a2fba05d4ef7849116d51d8cdf3f4358
Rishabh Aggarwal <ri.sbh23@GMAIL.COM>
2021-01-24 19:38:11 +0530

    Created Checkout path

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { LoginComponent } from './components/pages/login/login.component';
@@ -27,6 +28,11 @@ const routes: Routes = [
     path: 'dashboard',
     component: DashboardComponent
   }
+  ,
+{
+  path: 'checkout',
+    component: CheckoutComponent
+}
 ];
 
 @NgModule({
