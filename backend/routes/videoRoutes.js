85341b825d7c3ab05c54ceee19c30ecc5c2a8a8b
aadarshp31 <adarshpandey.in@gmail.com>
2021-01-24 16:46:46 +0530

    Added routes for videos

--- backend/routes/videoRoutes.js
+++ backend/routes/videoRoutes.js
@@ -4,6 +4,7 @@ const {
   getVideoById,
   getVideo,
   getAllVideos,
+  updateVideoById,
 } = require('../controllers/videoControllers')
 const { isAdmin, isSignedIn } = require('../middlewares/authMiddlewares')
 
@@ -12,8 +13,12 @@ const router = express.Router()
 router.param('videoId', getVideoById)
 
 router.route('/').get(getAllVideos).post(isSignedIn, isAdmin, createVideo)
+
 router.route('/admin').get(isSignedIn, isAdmin, getAllVideos)
-router.route('/:videoId').get(getVideo)
+router
+  .route('/:videoId')
+  .get(getVideo)
+  .put(isSignedIn, isAdmin, updateVideoById)
 router.route('/:videoId/admin').get(isSignedIn, isAdmin, getVideo)
 
 module.exports = router
