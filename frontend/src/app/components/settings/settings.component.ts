2b7fc4d5478a6eb70e35cc795c87c6d6fbd16888
deepak thakur <thakurdeepak675@gmail.com>
2021-01-25 15:13:38 +0530

    updated settings component

--- frontend/src/app/components/settings/settings.component.ts
+++ frontend/src/app/components/settings/settings.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import{UsersService} from 'src/app/services/users/users.service';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: UsersService) { }
 
+  userData:any;
+  userList: any;
+  selectedUser: any;
+  
   ngOnInit(): void {
+    this.service.getAllUser().subscribe((res: any) => {
+    this.userList = res;
+    })
   }
+  id="600ddfbf6229dd6df0040fe4";
+    selectedid(id){
+    this.service.getUser(id).subscribe((res: any)=>{
+      this.selectedUser=res;
+    })
+  }
+
+  onsave(){
+    this.service.updateUser(this.selectedUser,this.userData).subscribe((res)=>{
+
+  }) 
+}
+
+
+
 
 }
