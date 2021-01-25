2a4cb610ab3013ef531592b19a8ece0011650024
deepak thakur <thakurdeepak675@gmail.com>
2021-01-26 01:11:02 +0530

    deepak/ST-101_Create_Component_for_VideoManagement

--- frontend/src/app/components/videomanagement/videomanagement.component.ts
+++ frontend/src/app/components/videomanagement/videomanagement.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-videomanagement',
   templateUrl: './videomanagement.component.html',
-  styleUrls: ['./videomanagement.component.scss']
+  styleUrls: ['./videomanagement.component.scss'],
 })
+
 export class VideomanagementComponent implements OnInit {
 
-  constructor() { }
+   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
