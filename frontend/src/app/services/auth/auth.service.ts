0771461f45c7a695ccb4d9da295bcd1c68ef5835
aadarshp31 <adarshpandey.in@gmail.com>
2021-01-26 16:08:30 +0530

    Added 'isSignedIn' and 'isAdmin' middleware methods

--- frontend/src/app/services/auth/auth.service.ts
+++ frontend/src/app/services/auth/auth.service.ts
@@ -1,29 +1,48 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  url = "http://localhost:5000/api";
+  url: string = environment.backendApi;
 
   constructor(private http: HttpClient) { }
 
-  signup( payload ): Observable<any> {
+  signup(payload): Observable<any> {
     const tempUrl = this.url + "/signup";
     return this.http.post(tempUrl, payload);
   }
 
-  signin( email: string, password: string ): Observable<any> {
+  signin(email: string, password: string): Observable<any> {
     const tempUrl = this.url + "/signin";
     return this.http.post(tempUrl, { email, password });
   }
 
   logout(): void {
-    if ( window.localStorage && (localStorage.getItem('token') || localStorage.getItem('user')) ){
+    if (window.localStorage && localStorage.getItem('token')) {
       localStorage.removeItem('token');
+    }
+    if (window.localStorage && localStorage.getItem('user')) {
       localStorage.removeItem('user');
     }
+    return
+  }
+
+  isSignedIn(): Boolean {
+    if (window.localStorage && localStorage.getItem('token')) {
+      return true;
+    }
+    return false;
+  }
+
+  isAdmin(): Boolean {
+    if (window.localStorage && localStorage.getItem('user')) {
+      const user = JSON.parse(localStorage.getItem('user'))
+      return user.role === 1 ? true : false;
+    }
+    return false
   }
 }
