d0faecf89e813c0f0e5ed66c02ac1f7d58cee448
aadarshp31 <adarshpandey.in@gmail.com>
2021-01-26 20:41:58 +0530

    Added services for video

--- frontend/src/app/services/videos/videos.service.ts
+++ frontend/src/app/services/videos/videos.service.ts
@@ -24,7 +24,10 @@ export class VideosService {
   }
 
   getVideosAdmin(queryPaylaod = {}): Observable<any> {
-    const token = this.auth.isAdmin().subscribe(token => { return token })
+    if (!this.auth.isAdmin()) {
+      return
+    }
+    const token = this.auth.isAdmin()
     const headers = {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
@@ -35,13 +38,68 @@ export class VideosService {
   }
 
   getVideoByIdAdmin(id): Observable<any> {
-    const token = this.auth.isAdmin().subscribe(token => { return token })
+    if (!this.auth.isAdmin()) {
+      return
+    }
+    const token = this.auth.isAdmin()
     const headers = {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
       'Authorization': `Bearer ${token}`
     }
     return this.http.get(this.url + '/' + id + + '/admin', { headers })
+  }
+
+  getVideoStream(videoId, quality): Observable<any> {
+    if (this.auth.isSignedIn()) {
+      return
+    }
+    const token = this.auth.isSignedIn();
+    const headers = {
+      'Content-Type': 'application/json',
+      'Accept': '*/*',
+      'Authorization': `Bearer ${token}`
+    }
+    return this.http.get(this.url + `/stream/${quality}/${videoId}`)
+  }
+
+  // createVideo(formData): Observable<any> {
+  //   if (!this.auth.isAdmin()) {
+  //     return
+  //   }
+  //   const token = this.auth.isAdmin()
+  //   const headers = {
+  //     'Content-Type': 'multipart/form-data',
+  //     'Accept': 'application/json',
+  //     'Authorization': `Bearer ${token}`
+  //   }
+  //   return this.http.post(this.url, formData, { headers })
+  // }
+
+  // updateVideo(videoId, formData): Observable<any> {
+  //   if (!this.auth.isAdmin()) {
+  //     return
+  //   }
+  //   const token = this.auth.isAdmin()
+  //   const headers = {
+  //     'Content-Type': 'multipart/form-data',
+  //     'Accept': 'application/json',
+  //     'Authorization': `Bearer ${token}`
+  //   }
+  //   return this.http.put(this.url + videoId, formData, { headers })
+  // }
+
+  removeVideo(videoId): Observable<any> {
+    if (!this.auth.isAdmin()) {
+      return
+    }
+    const token = this.auth.isAdmin()
+    const headers = {
+      'Content-Type': 'multipart/form-data',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${token}`
+    }
+    return this.http.delete(this.url + videoId)
   }
 }
 
