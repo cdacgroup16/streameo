82aa7347cb76f0e56c6623b1b65122796f78b8f5
deepak thakur <thakurdeepak675@gmail.com>
2021-01-27 04:44:09 +0530

    integration done

--- frontend/src/app/components/categorylist/categorylist.component.ts
+++ frontend/src/app/components/categorylist/categorylist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from 'src/app/services/categories/categories.service';
 
 @Component({
   selector: 'app-categorylist',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategorylistComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private service: CategoriesService ) { }
+  categoryList : any;
   ngOnInit(): void {
+    this.service.getAllCat().subscribe((res) => {
+      this.categoryList = res;
   }
 
 }
