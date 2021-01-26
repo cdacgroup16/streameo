84ce66cbe328321b01a9ea03307753144f5917b6
deepak thakur <thakurdeepak675@gmail.com>
2021-01-27 04:30:16 +0530

    edit component added with itgration failure

--- frontend/src/app/components/editplan/editplan.component.ts
+++ frontend/src/app/components/editplan/editplan.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { Plans } from '../../entities/plans/plans';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-editplan',
@@ -7,9 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditplanComponent implements OnInit {
 
-  constructor() { }
+  name: String;
+  price :number;
+  concurrent_streams :number;
+  validity: number;
+  max_quality:number;
+  plansadd : Plans;
+  constructor(private  auth:AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
+  onSubmit(){
+    console.log(this.plansadd);
+    const payload = {
+    name: this.name,
+    price: this.price,
+    concurrent_streams: this.concurrent_streams,
+    validity: this.validity,
+    max_quality: this.max_quality
+}
+this.auth.signup(payload).subscribe(data => {
+
+  const { plansadd } = data;
+  this.plansadd = plansadd;
+  localStorage.setItem('plansadd', JSON.stringify(this.plansadd));
+  this.router.navigate(['/planlist']);
+},
+  err => {
+    console.error('plansadd failed \n', err.error?.message);
+  });
+
 
 }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
